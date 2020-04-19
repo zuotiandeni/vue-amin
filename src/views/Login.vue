7011ba9776e7dca5d222e4e4fb53c505327afb69
zuotiandeni <58815191+zuotiandeni@users.noreply.github.com>
2020-04-19 17:37:15 +0800

    项目创建、配置懒加载

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    <div>这里是登录页</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+  height: 100vh; // vh基于可视区的大小
+  background-color: #344a5f;
+}
+</style>
